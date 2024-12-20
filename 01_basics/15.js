fb2aecd15a9742473f1b1434ebd773f1cf8eef22
Arka Dyuti Pal <168279339+ArkaPal157@users.noreply.github.com>
2024-12-20 10:17:07 +0000

    New

---
+++ 01_basics/15.js
@@ -0,0 +1,17 @@
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+    }
+    // course.courseInstructor
+    
+    const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//    "name": "hitesh",
+//    "coursename": "js in hindi",
+//    "price": "free"
+// }
